f1911c1bdfbfd3ab32015e798bf593c26cfe061d
boobalannov <boobalanannamalai25@gmail.com>
2022-03-28 22:31:38 +0530

    new project

---
+++ src/card.java
@@ -0,0 +1,14 @@
+
+public class card {
+
+	// This is a simple Java program.
+	// FileName : "HelloWorld.java".
+
+	
+		// Your program begins with a call to main().
+		// Prints "Hello, World" to the terminal window.
+		public static void main(String args[])
+		{
+			System.out.println("Hello, World");
+		}
+	}
